51f18ffe6905e701e7a07800cf36ef6c944e9a56
Aayroon <aaronitez4@gmail.com>
2024-01-20 00:33:10 +0300

    Display sum, display cards and NEW CARD button

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message =""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
 function startGame() {
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    sumEl.textContent= "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card? "
     } else if (sum === 21) {
@@ -15,6 +20,11 @@ function startGame() {
         message = "You're out of the game!"
         isAlive = false
     }
-    
-    console.log(message)
+    messageEl.textContent = message
+}
+
+function newCard() {
+    let card = 5
+    sum += card
+    startGame()
 }
