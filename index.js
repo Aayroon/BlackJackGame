8f30de7a8df504bd9d7aa1cc416f0f5fa9b884a2
Aayroon <aaronitez4@gmail.com>
2024-02-08 14:29:48 +0300

    Assign values to startGame function, Modify newCard function

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message =""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
+
+player = {
+    name: "Jake",
+    chips: 150
+}
+
+playerEl.textContent = player.name + ": $" + player.chips
 
 function getRandomCard() {
     let randomCard = Math.floor(Math.random() * 13) + 1
@@ -21,6 +27,13 @@ function getRandomCard() {
 }
 
 function startGame() {
+    isAlive = true
+
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+
     renderGame()
 }
 
@@ -43,9 +56,12 @@ function renderGame() {
 }
 
 function newCard() {
-    let card = getRandomCard()
-    sum += card
-    cards.push(card)
-    renderGame()
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+
+        renderGame()
+    }
 }
 
